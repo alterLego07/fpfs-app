676b04779b69bf1c370c7893d1446ec1d7fd76c9
Emilio Garcia <emilioluis.lego1902@gmail.com>
2024-07-08 22:23:18 -0400

    Agregamos restricciones para la creacion de clubes y jugadores

--- app/Models/Clubes.php
+++ app/Models/Clubes.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 
 use App\Models\Ciudades;
 use App\Models\Federaciones;
@@ -31,5 +32,23 @@ class Clubes extends Model
     public function federacion(): BelongsTo
     {
         return $this->belongsTo(Federaciones::class);
+    }
+
+
+    public function scopeCustomSelect(Builder $query, $userId)
+    {
+        return $query->whereNotNull('created_at')
+                ->where(function($query) use ($userId) {
+                    $query->whereIn('federacion_id', function($query) use ($userId) {
+                        $query->select('federacion_id')
+                            ->from('federaciones_usuarios')
+                            ->where('user_id', $userId);
+                    })
+                    ->orWhere('id', function($query) use ($userId) {
+                        $query->select('club_id')
+                            ->from('clubes_usuarios')
+                            ->where('user_id', $userId);
+                    });
+                });
     }
 }
